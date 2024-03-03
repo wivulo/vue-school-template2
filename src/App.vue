826e3e7727ba8c125726829b06cdf00b8b14dc93
wivulo <walterivulo@gmail.com>
2024-03-03 13:45:25 +0100

    core: project initialized

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>{{ message }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: 'Hello, Vue 2!',
+        };
+    },
+};
+</script>
+
+<style lang="scss">
+
+</style>
